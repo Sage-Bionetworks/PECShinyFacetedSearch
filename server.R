dcd7a34edb6643a3f5c41a047d17afba9693440e
kdaily <kdaily@users.noreply.github.com>
2016-02-16 16:15:42 -0800

    add synapse specific stuff for auth

--- server.R
+++ server.R
@@ -2,16 +2,27 @@ library(shiny)
 library(DT)
 
 shinyServer(function(input, output) {
+  session$sendCustomMessage(type="readCookie",
+                            message=list(name='org.sagebionetworks.security.user.login.token'))
   
-  output$all <- DT::renderDataTable({
-    DT::datatable(allData, 
-                  extensions = c('ColReorder', 'ColVis'),
-                  filter = list(position = 'top', clear = FALSE),
-                  options = list(pageLength = 15,
-                                 dom='C<"clear">Rfrtip',
-                                 search = list(regex = TRUE),
-                                 autoWidth = TRUE),
-                  escape=1)
+  foo <- observeEvent(input$cookie, {
+    
+    synapseLogin(sessionToken=input$cookie)
+    
+    withProgress(message='Loading data...',
+                 {source("load.R")})
+    
+    output$all <- DT::renderDataTable({
+      DT::datatable(allData, 
+                    extensions = c('ColReorder', 'ColVis'),
+                    filter = list(position = 'top', clear = FALSE),
+                    options = list(pageLength = 15,
+                                   dom='C<"clear">Rfrtip',
+                                   search = list(regex = TRUE),
+                                   autoWidth = TRUE),
+                    escape=1)
+    })
+    
   })
-
+  
 })
